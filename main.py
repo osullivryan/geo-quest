f93d649fcb4591c15e26712169b5b7800ebf2d8d
Ryan O'Sullivan <osullivryan@gmail.com>
2021-05-21 12:53:59 -0700

    Add lots of scripting and sourcing

--- main.py
+++
@@ -1,8 +0,0 @@
-from get_location import GPSNotConnectedError, get_current_lat_lon
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
